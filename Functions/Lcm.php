407a9fbc51b3214709773e5f980ce2306f4bb1a6
Baptiste-R-epi <baptiste.royer@epitech.eu>
2024-02-21 15:42:44 +0100

    Solve Lcm bugs

--- Functions/Lcm.php
+++ Functions/Lcm.php
@@ -3,30 +3,50 @@ function math_lcm(...$inputs) {
 	$inputs = array_unique($inputs);
 	if (in_array(0, $inputs))
 		return 0;
+
+	foreach ($inputs as $key => $value) {
+		$value = intval($value);
+		if ($value == 1) {
+			unset($inputs[$key]);
+		} else {
+			$inputs[$key] = $value;
+		}
+	}
+
 	$multiple = false;
 	$lcm = 1;
+
 	do {
 		$multiple = false;
-		foreach ($inputs as &$value) {
+		foreach ($inputs as $key => $value) {
 			if ($value % 2 == 0) {
 				$value /= 2;
 				$multiple = true;
+				if ($value == 1)
+					unset($inputs[$key]);
 			}
 		}
+
 		if ($multiple)
 			$lcm *= 2;
 
 	} while ($multiple);
 
-	for ($i = 3; array_unique($inputs) != [1]; $i += 2) {
+	for ($i = 3; $inputs != []; $i += 2) {
 		do {
 			$multiple = false;
-			foreach ($inputs as &$value) {
+			foreach ($inputs as $key => $value) {
 				if ($value % $i == 0) {
 					$value /= $i;
 					$multiple = true;
+					if ($value == 1) {
+						unset($inputs[$key]);
+					} else {
+						$inputs[$key] = $value;
+					}
 				}
 			}
+
 			if ($multiple)
 				$lcm *= $i;
 
