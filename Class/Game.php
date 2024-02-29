52e91d7c8dcb65606b95bdbd78f1042e632bb63a
Baptiste-R-epi <baptiste.royer@epitech.eu>
2024-02-29 14:31:40 +0100

    add end score screen (with animation)

--- Class/Game.php
+++ Class/Game.php
@@ -37,12 +37,13 @@ class Game
 
 				if ($this->Level_board->isLost()) {
 					$this->player->setPosition($this->Level_board->starting_position);
-					$this->Level_board->Score--;
+					$this->Level_board->Score -= 2;
 				}
 
 				if ($order == "ESC") {
-					break 2;
+					return false;
 				}
+				
 				$this->Level_board->play($order);
 				$this->Level_board->displayBoard();
 				$this->config->display_data_next_cycle();
@@ -50,6 +51,73 @@ class Game
 			}
 			$this->level++;
 		}
+		return true;
+	}
+
+	function end_congrate() {
+		// Clear screen
+		echo "\e[H\e[J";
+
+		// Draw Congrate
+		echo "\e[3;10H";
+		echo "CONGRATULATION !";
+
+		echo "\e[4;6H";
+		echo "YOU BEAT ALL THE LEVELS";
+
+		// Draw score
+		echo "\e[6;10H";
+		echo "YOUR SCORE : " . $this->Level_board->Score;
+
+		// Draw press key
+		echo "\e[14;1H";
+		echo "PRESS ENTER OR ESC TO END";
+
+		do {
+			usleep(100000);
+			$order = $this->config->get_order();
+			$this->config->display_data_next_cycle();
+
+			// Store variables
+			$border = $this->config->get_display_char_from_type("BORDER");
+			$block = $this->config->get_display_char_from_type("BLOCK");
+			$player = $this->config->get_display_char_from_type("CURSOR");
+			$coin = $this->config->get_display_char_from_type("SCORE_COIN");
+			$spike = $this->config->get_display_char_from_type("SPIKE");
+
+			// Draw upper border
+			echo "\e[1;1H";
+			echo str_repeat($border, 33);
+
+			// Draw left & right border
+			for ($i = 2; $i <= 11; $i++) {
+				echo "\e[" . $i . ";1H";
+				echo $border;
+				echo "\e[" . $i . ";33H";
+				echo $border;
+			}
+
+			// Draw lower border
+			echo "\e[12;1H";
+			echo str_repeat($border, 33);
+
+			// Draw stage
+			echo "\e[8;15H";
+			echo $coin . $coin . $player . $coin . $coin;
+
+			echo "\e[9;14H";
+			echo str_repeat($block, 7);
+
+			echo "\e[10;13H";
+			echo $block;
+			echo "\e[10;21H";
+			echo $block;
+
+			echo "\e[11;13H";
+			echo $block . str_repeat($spike, 7) . $block;
+
+			echo "\e[16;13H";
+		} while ($order != "ENTER" && $order != "ESC");
 	}
 
 	public function start_game() {
@@ -57,7 +125,10 @@ class Game
 		system("stty cbreak -echo");
 		echo "\e[?25l";
 
-		$this->play_game();
+		if ($this->play_game()) {
+			$this->end_congrate();
+		}
+
 		system("stty cbreak echo");
 		echo "\e[?25h\n\e[A";
 	}
